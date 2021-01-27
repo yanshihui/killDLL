5121af1a9432b50c9fc197257a048bfc5037dafe
Xylon <x956200361@gmail.com>
2021-01-28 00:17:22 +0800

    drop access task method

--- android-side/app/src/main/java/com/example/killdll/storageSDK/local/DBOperator.java
+++ android-side/app/src/main/java/com/example/killdll/storageSDK/local/DBOperator.java
@@ -1,5 +1,6 @@
 package com.example.killdll.storageSDK.local;
 
+import com.alibaba.fastjson.JSON;
 import com.example.killdll.storageSDK.entity.DBTask;
 import com.example.killdll.storageSDK.entity.Task;
 
@@ -22,16 +23,12 @@ public class DBOperator {
         db.taskDAO().insertManyDBTasks(dbTasks);
     }
 
-    public static void deleteManyTasks(AppDatabase db, List<Task> tasks) {
-        List<DBTask> dbTasks = new ArrayList<>();
-        for (Task task : tasks) {
-            dbTasks.add(new DBTask(task));
-        }
-        db.taskDAO().deleteManyDBTasks(dbTasks);
+    public static void deleteOneTaskById(AppDatabase db, String id){
+        db.taskDAO().deleteDBTaskById(id);
     }
 
-    public static void deleteOneTask(AppDatabase db, Task task) {
-        db.taskDAO().deleteOneDBTask(new DBTask(task));
+    public static void deleteManyTasksById(AppDatabase db, List<String> ids){
+        db.taskDAO().deleteDBTasksByIds(ids);
     }
 
     public static void updateOneTask(AppDatabase db, Task task) {
@@ -47,11 +44,42 @@ public class DBOperator {
         return tasks;
     }
 
-    public static List<Task> queryAllTasks(AppDatabase db){
+    public static Task queryTaskById(AppDatabase db, String id) {
+        return db.taskDAO().getDBTaskById(id).getTask();
+    }
+
+    public static List<Task> queryTasksByRestTimeInc(AppDatabase db){
+        List<Task> tasks = new ArrayList<>();
+        List<DBTask> dbTasks = db.taskDAO().getAllDBTasksOrderByRestTimeInc(System.currentTimeMillis());
+        for(DBTask dbTask : dbTasks){
+            tasks.add(dbTask.getTask());
+        }
+        return tasks;
+    }
+
+    public static List<Task> queryAllTasks(AppDatabase db) {
         List<Task> tasks = new ArrayList<>();
         List<DBTask> dbTasks = db.taskDAO().getAllDBTasks();
-        for (DBTask dbTask : dbTasks){
+        for (DBTask dbTask : dbTasks) {
             tasks.add(dbTask.getTask());
+        }
+        return tasks;
+    }
+
+    public static List<List<Double>> queryAllScheduleAllocation(AppDatabase db) {
+        List<List<Double>> allocations = new ArrayList<>();
+        List<String> jsonData = db.taskDAO().getAllScheduleAllocation();
+        for (String json : jsonData) {
+            allocations.add(JSON.parseArray(json, Double.class));
+        }
+        return allocations;
+    }
+
+    public static List<Task> queryAllTaskNamesByState(AppDatabase db, String taskState) {
+        List<Task> tasks = new ArrayList<>();
+        List<TaskDAO.NameId> NameIds = db.taskDAO().getAllTaskNamesByState(taskState);
+        for (TaskDAO.NameId nameId : NameIds) {
+            tasks.add(new Task(nameId.id, nameId.name));
         }
         return tasks;
     }
